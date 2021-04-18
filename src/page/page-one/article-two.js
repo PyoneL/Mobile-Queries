4e859f34e47a8609720a019553a6313afb4f6fda
Engin Yenice <enginyenice2626@gmail.com>
2021-04-18 06:03:30 +0300

    TypeOneArticleTwo Post added.

--- src/page/page-one/article-two.js
+++ src/page/page-one/article-two.js
@@ -1,47 +1,144 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { View, StyleSheet, ScrollView } from 'react-native';
-import Constants from 'expo-constants';
-import { Container, Header, Content, Card, CardItem, Text, Body } from 'native-base';
+import {
+  Badge,
+  Body,
+  CardItem,
+  Col,
+  Container,
+  Content,
+  Row,
+  Text,
+  View,
+} from "native-base";
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  TextInput,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { Card } from "react-native-paper";
+import { getTypeOneArticleTwo } from "../../services/fetch-services";
 
-const ArticleTwo = () => {
+export default class articleTwo extends Component {
+  state = {
+    distance: Number,
+    Data: Array,
+    Success: Boolean,
+    Message: String,
+  };
+  constructor(props) {
+    super(props);
+    this.setDistance = this.setDistance.bind(this);
+  }
+  setDistance(text) {
+    console.log(text);
+    this.setState({ distance: text });
+  }
+  getData = () => {
+    getTypeOneArticleTwo(this.state.distance).then((x) => {
+      this.setState({ Data: x.data });
+      this.setState({ Success: x.success });
+      this.setState({ Message: x.message });
+    });
+  };
+  /*
+ {puDatetime: "2020-12-16T16:35:05", trip_distance: 4.99}
+puDatetime: "2020-12-16T16:35:05"
+trip_distance: 4.99
+*/
+
+  getView() {
+    let data = this.state.Data.map((value, key) => (
+      <Card padder>
+        <CardItem cardBody>
+          <Content>
+            <Col>
+              <Text>puDatetime : </Text>
+            </Col>
+            <Col>
+              <Badge info>
+                <Text>{value.puDatetime}</Text>
+              </Badge>
+            </Col>
+
+            <Col>
+              <Text>trip_distance : </Text>
+            </Col>
+            <Col>
+              <Badge warning>
+                <Text>{value.trip_distance}</Text>
+              </Badge>
+            </Col>
+          </Content>
+        </CardItem>
+      </Card>
+    ));
+
+    return <View>{data}</View>;
+  }
+  render() {
     return (
-      <Container style={styles.container}>
-        <View  style={styles.body}>   
-          <View style={styles.header}>
-            <Card>
-                <CardItem style={{backgroundColor:'#e85f5f'}}>
+      <>
+        <Container>
+          <View>
+            <View>
+              <Card>
+                <CardItem style={{ backgroundColor: "#e85f5f" }}>
                   <Body>
-                    <Text style={{fontSize:20, color:'white', fontWeight:'bold'}}>Belirli mesafenin altında en çok seyahat yapılan gün ve seyahat uzunluğu</Text>
+                    <Text
+                      style={{
+                        fontSize: 20,
+                        color: "white",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      Belirli mesafenin altında en çok seyahat yapılan gün ve
+                      seyahat uzunluğu
+                    </Text>
                   </Body>
                 </CardItem>
-            </Card>             
+              </Card>
+              <Card>
+                <CardItem>
+                  <SafeAreaView>
+                    <TextInput
+                      style={styles.input}
+                      onChangeText={this.setDistance}
+                      placeholder="useless placeholder"
+                      keyboardType="numeric"
+                    />
+                    <TouchableOpacity
+                      style={styles.button}
+                      onPress={this.getData}
+                    >
+                      <Text>Press Here</Text>
+                    </TouchableOpacity>
+                  </SafeAreaView>
+                </CardItem>
+              </Card>
+            </View>
           </View>
           <ScrollView>
-            <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
-              <Text>TO DO</Text>
-            </View>
-          </ScrollView>
-        </View>
-        <StatusBar style="light" />
-      </Container>
+                {this.state.Data != null &&
+                  this.state.Success == true &&
+                  this.getView()}
+              </ScrollView>
+        </Container>
+      </>
     );
-};
-export default ArticleTwo;
+  }
+}
 
 const styles = new StyleSheet.create({
-  container:{
-      flex:1,
-      flexDirection:'row',
-      justifyContent:'center',
-      backgroundColor:'#deded5'
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
   },
-  body:{ 
-      flex:1,
-      padding:10, 
-      paddingBottom:30,
-  },
-  header:{
-      marginBottom:10,
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10,
   },
 });
--- src/services/fetch-services.js
+++ src/services/fetch-services.js
@@ -9,8 +9,6 @@ const getTypeOneArticleOne = () => {
         console.error(error);
       });
   };
-
-
   const getTypeOneArticleThree = () => {
     return fetch(apiURL+'ExampleOneQueries/queryThree')
       .then((response) => response.json())
@@ -21,6 +19,21 @@ const getTypeOneArticleOne = () => {
         console.error(error);
       });
   };
+  const getTypeOneArticleTwo = (distance) => {
+    return fetch(apiURL+"ExampleOneQueries/queryTwo", {
+      method:"POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({distance: distance})
+    }).then((response) => response.json())
+    .then((json) => {
+      return  json;
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
 
 
   const getTypeThreeArticleThree = () => {
@@ -37,4 +50,4 @@ const getTypeOneArticleOne = () => {
 
 
 
-export {getTypeOneArticleOne, getTypeOneArticleThree,getTypeThreeArticleThree};
+export {getTypeOneArticleOne,getTypeOneArticleTwo, getTypeOneArticleThree,getTypeThreeArticleThree};
